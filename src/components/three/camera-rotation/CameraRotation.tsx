68dc2917a10a533b5403b992858ff1cda9d9a0b5
radziminski <radziminski.j@gmail.com>
2023-07-02 17:59:30 +0200

    fix(Camera): fixed camera guesses

--- src/components/three/camera-rotation/CameraRotation.tsx
+++ src/components/three/camera-rotation/CameraRotation.tsx
@@ -1,37 +1,23 @@
-import { useFrame } from '@react-three/fiber';
-import { MathUtils } from 'three';
+import { useCamera } from '@react-three/drei';
+import { useFrame, useThree } from '@react-three/fiber';
+import { MathUtils, Vector3 } from 'three';
+import { radToDeg } from 'three/src/math/MathUtils';
+import { roundToDecimal } from '~/helpers/3D/getUnitSphereCoordinates';
 import { useSettingsStore } from '~/store/settings/useSettingsStore';
 
 export const CameraRotation = () => {
   const { azimuth, elevation, setAzimuth, setElevation } = useSettingsStore();
 
   useFrame(({ camera }) => {
-    const cameraRotationX = camera.rotation.x;
-    const cameraRotationY = camera.rotation.y;
-    const cameraRotationZ = camera.rotation.z;
+    var vector = new Vector3(); // create once and reuse it!
+    const { x, y, z } = camera.getWorldDirection(vector);
+    const azimuthDegree = radToDeg(Math.atan2(x, z)) + 180;
+    const azimuth = roundToDecimal(360 - azimuthDegree);
 
-    const elevation = cameraRotationX * 57.29;
-
-    let angle = cameraRotationY * MathUtils.RAD2DEG;
-
-    if (cameraRotationZ < 1.5 && cameraRotationY < 0) {
-      angle = -angle;
-    }
-
-    if (cameraRotationZ < 1.5 && cameraRotationY > 0) {
-      angle = 360 - angle;
-    }
-
-    if (cameraRotationZ > 1.5 && cameraRotationY < 0) {
-      angle = 180 + -angle;
-    }
-
-    if (cameraRotationZ > 1.5 && cameraRotationY > 0) {
-      angle = 180 + angle;
-    }
+    const elevation = radToDeg(Math.asin(y));
 
     (window as any).elevation = elevation;
-    (window as any).azimuth = angle;
+    (window as any).azimuth = azimuth;
   });
 
   return null;
