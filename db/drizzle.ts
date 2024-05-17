a7c014a073dc4075daff4f738e27c0367aa1bc77
radziminski <radziminski.j@gmail.com>
2024-05-17 13:49:56 +0200

    fix(db): fixed comment typo

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -10,7 +10,7 @@ export const db = drizzle(sql, { schema });
  * After the survey was completed on 7.05.2024, the Vercel free-tier limits were exceeded (on 15.05.2024)
  * and the connections to the database were limited until the end of the month.
  *
- * To keep the preview of the application running, I've switched the database a different provider (since
+ * To keep the preview of the application running, I've switched the database to a different provider (since
  * the survey were already finished, it did not have any impact on its progress or results).
  */
 import { drizzle } from 'drizzle-orm/postgres-js';
